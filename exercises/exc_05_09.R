025c37602f4451dbb3e931ce49551ce2a635c18d
Barry digby <b.digby237@gmail.com>
2021-12-01 16:30:13 +0000

    df

--- exercises/exc_05_09.R
+++ exercises/exc_05_09.R
@@ -3,12 +3,13 @@ library(caret)
 # load training and test object created in previous step:
 train <- read.delim("https://raw.githubusercontent.com/BarryDigby/Youth-Academy/master/data/train.txt", row.names="samples", header=T, sep="\t", stringsAsFactors = TRUE)
 test <- read.delim("https://raw.githubusercontent.com/BarryDigby/Youth-Academy/master/data/test.txt", row.names="samples", header=T, sep="\t", stringsAsFactors = TRUE)
+
 # re-make training model 
 #      knn3(training data, training labels, value for k)
 model_fit <- knn3(train[_:__], train[,__], k = _)
 
 # make predictions
-# insert 'training data' below
+# insert 'test data' below
 test_predictions <- predict(model_fit, test[,_:__], type = "class")
 
 # evaluate model
